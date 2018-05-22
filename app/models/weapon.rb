222a076c34c8c21333ca807eb6911411e81edd71
Yuta Goto <you.goto.510@gmail.com>
2018-05-23 00:37:45 +0900

    画像のURL追加 (#6)

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -4,6 +4,7 @@
 #
 #  id         :bigint(8)        not null, primary key
 #  name       :string           not null
+#  image_url  :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
--- db/migrate/20180306160202_create_stages.rb
+++ db/migrate/20180306160202_create_stages.rb
@@ -2,6 +2,7 @@ class CreateStages < ActiveRecord::Migration[5.1]
   def change
     create_table :stages do |t|
       t.string :name, null: false
+      t.string :image_url
 
       t.timestamps
     end
