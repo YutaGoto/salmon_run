b2c7ed6785adc01a06b43ce5852135d1c1d2b648
YutaGoto <you.goto.510@gmail.com>
2018-07-16 22:09:56 +0900

    config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module SalmonRun
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
