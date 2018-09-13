f71371c699c2397837f849bc1cf8a9e3ee5caf2a
YutaGoto <you.goto.510@gmail.com>
2018-09-13 15:45:29 +0900

    todo reek追加し対応

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all.includes(:stage, :weapons).order(:id)
-    @opening_event = @events.opening.last
+    @opening_event = Event.opening.last
   end
 end
