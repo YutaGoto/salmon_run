27bc6e61758955b94f41eebe1f460b79b3baa2b2
YutaGoto <you.goto.510@gmail.com>
2018-09-24 16:41:56 +0900

    不要なspec削除

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe WelcomeController, type: :controller do
+RSpec.describe EventsController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
