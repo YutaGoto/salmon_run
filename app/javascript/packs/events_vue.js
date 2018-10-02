4469189c2f46a597dfc0848b010caf7438084aa2
YutaGoto <you.goto.510@gmail.com>
2018-10-02 09:23:38 +0900

    sap準備

--- app/javascript/packs/events_vue.js
+++ app/javascript/packs/events_vue.js
@@ -1,24 +1,25 @@
 /* eslint no-console: 0 */
 // import Vue from 'vue'
-// import App from '../app.vue'
+
+// import Index from '../components/index.vue'
 
 import Vue from 'vue/dist/vue.esm'
+import Router from './router/router'
 import axios from 'axios';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    el: "#openingEvent",
-    data: function() {
-      return {
-        isOpen: false,
-        opening_event: {},
-      }
-    },
-    mounted () {
-      axios.get('/api/events/open').then(res => {
-        this.isOpen = res.data.data.is_open
-        this.opening_event = res.data.data.event
-      })
-    }
-  });
-})
+const app = new Vue({
+  // el: "#salmon",
+
+  // data: function() {
+  //   return {
+  //     isOpen: false,
+  //     opening_event: {},
+  //   }
+  // },
+  // mounted () {
+  //   axios.get('/api/events/open').then(res => {
+  //     this.isOpen = res.data.data.is_open
+  //     this.opening_event = res.data.data.event
+  //   })
+  // }
+});
