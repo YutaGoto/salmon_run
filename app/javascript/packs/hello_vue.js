9f96f5187723d6f4039822eeb2396918555afc07
YutaGoto <you.goto.510@gmail.com>
2018-11-01 17:59:20 +0900

    webpack のバージョンを下げる

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -5,30 +5,17 @@
 // like app/views/layouts/application.html.erb.
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
 
-// import Vue from 'vue'
-// import App from '../app.vue'
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const el = document.body.appendChild(document.createElement('hello'))
-//   const app = new Vue({
-//     el,
-//     render: h => h(App)
-//   })
-
-//   console.log(app)
-// })
-
-import Vue from 'vue/dist/vue.esm'
+import Vue from 'vue'
 import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
+  const el = document.body.appendChild(document.createElement('hello'))
   const app = new Vue({
-    el: '#hello',
-    data: {
-      message: "Can you say hello?"
-    },
-    components: { App }
+    el,
+    render: h => h(App)
   })
+
+  console.log(app)
 })
 
 
