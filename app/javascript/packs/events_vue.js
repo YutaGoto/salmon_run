a594feab862377ed84f9f871ca254876ca8f1092
YutaGoto <you.goto.510@gmail.com>
2018-11-28 00:47:12 +0900

    APIバグ修正

--- app/javascript/packs/events_vue.js
+++ app/javascript/packs/events_vue.js
@@ -20,7 +20,7 @@ var app = new Vue({
   },
   mounted () {
     axios.get("/api/events/open").then((res) => {
-      this.isOpen = res.data.data.is_open
+      this.isOpen = res.data.data.is_open;
       if (this.isOpen) {
         this.openingEvent = res.data.data.event;
       }
