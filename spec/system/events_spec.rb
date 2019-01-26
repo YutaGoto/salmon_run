a18c629765027318135e8264407aaaf9cccbcd0f
YutaGoto <you.goto.510@gmail.com>
2019-01-26 18:56:34 +0900

    import

--- spec/system/events_spec.rb
+++ spec/system/events_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe 'Events', type: :system, elasticsearch: true do
+  before do
+    Weapon.create_index!
+    Weapon.import
+  end
+
   context 'with show action' do
     it 'To show h1 content' do
       events_weapon = FactoryBot.create :events_weapon
