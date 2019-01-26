c2754786686938f54fd9fa1b177958c22c564f9b
YutaGoto <you.goto.510@gmail.com>
2019-01-26 22:34:15 +0900

    options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -103,14 +103,14 @@ RSpec.configure do |config|
 
   # elasticsearch config
   config.before(:all, elasticsearch: true) do
-    unless Elasticsearch::Extensions::Test::Cluster.running?
-      Elasticsearch::Extensions::Test::Cluster.start(command: '~/elasticsearch-5.6.14/bin/elasticsearch', port: 9250, number_of_nodes: 1)
+    unless Elasticsearch::Extensions::Test::Cluster.running?(command: '~/elasticsearch-5.6.14/bin/elasticsearch', number_of_nodes: 1)
+      Elasticsearch::Extensions::Test::Cluster.start(command: '~/elasticsearch-5.6.14/bin/elasticsearch', number_of_nodes: 1)
     end
   end
 
   config.after(:all, elasticsearch: true) do
-    if Elasticsearch::Extensions::Test::Cluster.running?
-      Elasticsearch::Extensions::Test::Cluster.stop(command: '~/elasticsearch-5.6.14/bin/elasticsearch', port: 9250, number_of_nodes: 1)
+    if Elasticsearch::Extensions::Test::Cluster.running?(command: '~/elasticsearch-5.6.14/bin/elasticsearch', number_of_nodes: 1)
+      Elasticsearch::Extensions::Test::Cluster.stop(command: '~/elasticsearch-5.6.14/bin/elasticsearch', number_of_nodes: 1)
     end
   end
 
