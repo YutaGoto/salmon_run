619c446df6f31f434e07be3da59927fd6e37a74a
YutaGoto <you.goto.510@gmail.com>
2019-01-26 23:03:31 +0900

    version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -103,11 +103,15 @@ RSpec.configure do |config|
 
   # elasticsearch config
   config.before(:all, elasticsearch: true) do
-    Elasticsearch::Extensions::Test::Cluster.start(command: '~/elasticsearch-5.6.14/bin/elasticsearch', number_of_nodes: 1)
+    unless Elasticsearch::Extensions::Test::Cluster.running?(command: '~/elasticsearch-5.6.14/bin/elasticsearch', number_of_nodes: 1, version: '5.6.14')
+      Elasticsearch::Extensions::Test::Cluster.start(command: '~/elasticsearch-5.6.14/bin/elasticsearch', number_of_nodes: 1, version: '5.6.14')
+    end
   end
 
   config.after(:all, elasticsearch: true) do
-    Elasticsearch::Extensions::Test::Cluster.stop(command: '~/elasticsearch-5.6.14/bin/elasticsearch', number_of_nodes: 1)
+    if Elasticsearch::Extensions::Test::Cluster.running?(command: '~/elasticsearch-5.6.14/bin/elasticsearch', number_of_nodes: 1, version: '5.6.14')
+      Elasticsearch::Extensions::Test::Cluster.stop(command: '~/elasticsearch-5.6.14/bin/elasticsearch', number_of_nodes: 1, version: '5.6.14')
+    end
   end
 
   Capybara.register_driver :selenium do |app|
