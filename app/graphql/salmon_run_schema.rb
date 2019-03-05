c4672457f4374d40eace17b7adea0ad3016dbb43
YutaGoto <you.goto.510@gmail.com>
2019-03-05 18:44:40 +0900

    graphql

---
+++ app/graphql/salmon_run_schema.rb
@@ -0,0 +1,4 @@
+class SalmonRunSchema < GraphQL::Schema
+  mutation(Types::MutationType)
+  query(Types::QueryType)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
   resources :events, only: %i[index show]
   resources :stages, only: %i[show]
 
