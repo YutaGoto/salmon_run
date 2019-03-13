b3c0a534d04d2eca136ba610bf29ecb6ec9b80bc
YutaGoto <you.goto.510@gmail.com>
2019-03-13 13:02:30 +0900

    events type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,8 +5,16 @@ module Types
       argument :id, ID, required: true
     end
 
+    field :events, [EventType], null: true do
+      description 'get all events'
+    end
+
     def event(id:)
       Event.find(id)
+    end
+
+    def events
+      Event.all
     end
   end
 end
