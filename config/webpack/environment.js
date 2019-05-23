df062f7e0158fcd12f3b4cab5c35482763835955
YutaGoto <you.goto.510@gmail.com>
2019-05-23 19:45:32 +0900

    :green_heart: add eslint

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,5 @@
 const { environment } = require("@rails/webpacker");
-const vue =  require("./loaders/vue");
+const eslint =  require('./loaders/eslint')
 
 environment.loaders.append("vue", vue)
 module.exports = environment;
