7678072a8386f0cb87b28f77a1998c581cbf3cf5
YutaGoto <you.goto.510@gmail.com>
2019-07-16 01:17:51 +0900

    fix config

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || "production";
 
 const environment = require("./environment");
 
-module.exports = environment.toWebpackConfig();
+const environment = require('./environment');
+
+module.exports = Object.assign({}, environment.toWebpackConfig(), {
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+});
--- config/webpack/test.js
+++ config/webpack/test.js
@@ -2,4 +2,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 const environment = require("./environment");
 
-module.exports = environment.toWebpackConfig();
+const environment = require('./environment');
+
+module.exports = Object.assign({}, environment.toWebpackConfig(), {
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+});
