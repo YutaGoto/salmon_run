351d9ec42a564287e79ac5f90297172deeda03a7
YutaGoto <you.goto.510@gmail.com>
2019-08-30 20:08:27 +0900

    bundle exec rails app:update

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,17 @@ module SalmonRun
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
     config.i18n.default_locale = :ja
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
     config.active_record.default_timezone = :local
     config.time_zone = 'Tokyo'
     config.assets.initialize_on_precompile = false
     config.generators.javascript_engine = :js
-
     config.weapon_index_analysis = config_for(:weapon_index_analysis)
   end
 end
